c51512fa9d77d36de7add9a403ac61dc4cb0accd
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-09-09 07:42:47 +0100

    Made protocol methods abstract

--- jetblack_finance/pnl/itrade.py
+++ jetblack_finance/pnl/itrade.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from abc import abstractmethod
 from decimal import Decimal
 from typing import Protocol
 
@@ -9,12 +10,15 @@ from typing import Protocol
 class ITrade(Protocol):
 
     @property
+    @abstractmethod
     def quantity(self) -> Decimal:
         ...
 
     @property
+    @abstractmethod
     def price(self) -> Decimal:
         ...
 
+    @abstractmethod
     def make_trade(self, quantity: Decimal) -> ITrade:
         ...
